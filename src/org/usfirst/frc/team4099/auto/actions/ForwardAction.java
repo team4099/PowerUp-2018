f86fbb4948cacddd08609b9d9d777b6163cfd32d
Parth Oza <plusparth@gmail.com>
2017-02-16 22:38:21 -0500

    Populate ForwardAction

--- src/org/usfirst/frc/team4099/auto/actions/ForwardAction.java
+++ src/org/usfirst/frc/team4099/auto/actions/ForwardAction.java
@@ -1,7 +1,38 @@
 package org.usfirst.frc.team4099.auto.actions;
 
+import org.usfirst.frc.team4099.robot.subsystems.Drive;
+
 /**
  * Created by plato2000 on 2/16/17.
  */
-public class ForwardAction {
+public class ForwardAction implements Action {
+    private Drive mDrive;
+    private double metersForward;
+    private boolean isDone;
+
+    public ForwardAction(double metersForward) {
+        this.mDrive = Drive.getInstance();
+        this.metersForward = metersForward;
+        this.isDone = false;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return isDone;
+    }
+
+    @Override
+    public void update() {
+        isDone = mDrive.goForward();
+    }
+
+    @Override
+    public void done() {
+        mDrive.finishForward();
+    }
+
+    @Override
+    public void start() {
+        mDrive.setForwardSetpoint(metersForward);
+    }
 }
