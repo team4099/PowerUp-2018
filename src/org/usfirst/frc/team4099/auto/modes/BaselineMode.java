30fb31030b619aeba1210ee7bb44c99aeee188f1
Oksana <tkachoksn@gmail.com>
2017-02-17 16:11:15 -0500

    Baseline Mode

--- src/org/usfirst/frc/team4099/auto/modes/BaselineMode.java
+++ src/org/usfirst/frc/team4099/auto/modes/BaselineMode.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team4099.auto.modes;
 
+// go to the baseline
+
 import org.usfirst.frc.team4099.auto.AutoModeEndedException;
-import org.usfirst.frc.team4099.auto.actions.TurnAction;
+import org.usfirst.frc.team4099.auto.actions.*;
 import org.usfirst.frc.team4099.lib.util.AutonomousInitParameters;
 import org.usfirst.frc.team4099.lib.util.Rotation2D;
 
@@ -11,14 +13,26 @@ import org.usfirst.frc.team4099.lib.util.Rotation2D;
 public class BaselineMode extends AutoModeBase {
     private final double initialForwardDistance;
     private final boolean turnAround;
+    private double turningAngle;
 
-    public BaselineMode(AutonomousInitParameters initParameters, boolean turnAround) {
+    public BaselineMode(AutonomousInitParameters initParameters, boolean turnAround, double Angle) {
         this.turnAround = turnAround;
         this.initialForwardDistance = initParameters.getDistanceInMeters();
+        this.turningAngle = initParameters.getTurnAngle().getDegrees();
     }
 
     @Override
     protected void routine() throws AutoModeEndedException {
-        runAction(new TurnAction(Rotation2D.fromDegrees(90)));
+        if (turningAngle > 0 || turningAngle < 0){
+            runAction(new ForwardAction(initialForwardDistance));
+        }else{
+            runAction(new TurnAction(Rotation2D.fromDegrees(90)));
+            runAction(new ForwardAction(1.37)); // distance in meters
+            runAction(new TurnAction(Rotation2D.fromDegrees(-90)));
+            runAction(new ForwardAction(initialForwardDistance));
+        }
+
     }
+
+
 }
