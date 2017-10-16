98713c22de53c90c19f835cd3cf5c3d5c70d454b
Parth Oza <plusparth@gmail.com>
2017-10-15 20:10:18 -0400

    Switch back to competition config

--- src/main/java/org/usfirst/frc/team4099/robot/ControlBoard.kt
+++ src/main/java/org/usfirst/frc/team4099/robot/ControlBoard.kt
@@ -1,11 +1,12 @@
 package org.usfirst.frc.team4099.robot
 
+import org.usfirst.frc.team4099.lib.joystick.DualShock4Gamepad
 import org.usfirst.frc.team4099.lib.joystick.JoystickUtils
 import org.usfirst.frc.team4099.lib.joystick.LogitechF310Gamepad
 
 class ControlBoard private constructor() {
     private val driver: LogitechF310Gamepad = LogitechF310Gamepad(Constants.Joysticks.DRIVER_PORT)
-//    private val operator: DualShock4Gamepad = DualShock4Gamepad(Constants.Joysticks.SHOTGUN_PORT)
+    private val operator: DualShock4Gamepad = DualShock4Gamepad(Constants.Joysticks.SHOTGUN_PORT)
 
     val throttle: Double
         get() = -driver.rightTriggerAxis + driver.leftTriggerAxis
@@ -25,19 +26,19 @@ class ControlBoard private constructor() {
         get() = Math.abs(JoystickUtils.deadbandNoShape(throttle, 0.02)) < 0.01
 
     val intakeUp: Boolean
-        get() = driver.dPadUp
+        get() = operator.dPadUp
 
     val intakeDown: Boolean
-        get() = driver.dPadDown
+        get() = operator.dPadDown
 
     val toggleIntake: Boolean
-        get() = driver.aButton
+        get() = operator.aButton
 
     val climber: Boolean
-        get() = driver.yButton
+        get() = operator.yButton
 
     val toggleIntakeClosed: Boolean
-        get() = driver.dPadRight
+        get() = operator.dPadRight || operator.dPadLeft
 
     companion object {
         val instance = ControlBoard()
