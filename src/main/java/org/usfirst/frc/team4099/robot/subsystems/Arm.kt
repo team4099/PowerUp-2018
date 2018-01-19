a38ca8e94cc356d6aad10ea10931c1e2a57a6cf7
Rithvik <rbhogavilli@gmail.com>
2018-01-19 16:51:17 -0500

    Finish documentation for arm subsystem

--- src/main/java/org/usfirst/frc/team4099/robot/subsystems/Arm.kt
+++ src/main/java/org/usfirst/frc/team4099/robot/subsystems/Arm.kt
@@ -63,14 +63,28 @@ class Arm private constructor() : Subsystem {
         }
     }
 
+    /**
+     * Sets the control mode to Velocity Control or Movement Control
+     *
+     * @param velocityControl a Boolean for setting to Velocity Control
+     */
     fun velocityControlSwitch(velocityControl: Boolean) {
         useVelocityControl = velocityControl
     }
 
+    /**
+     * Changes the power to the Arm
+     *
+     * @param power A double for the power going to the arm
+     */
     private fun setArmPower(power: Double) {
         masterSRX.set(ControlMode.MotionMagic, Math.abs(power))
     }
 
+    /**
+     * Outputs arm subsystem
+     *
+     */
     override fun outputToSmartDashboard() {
         SmartDashboard.putNumber("armPower", armPower)
     }
@@ -80,11 +94,19 @@ class Arm private constructor() : Subsystem {
         setArmPower(0.0)
     }
 
+    /**
+     * Handles power to arm during each loop cycle
+     *
+     * @constructor Creates loop that controls power to arm during each loop cycle
+     */
     val loop: Loop = object : Loop {
         override fun onStart() {
             setArmPower(0.0)
         }
 
+        /**
+         * Handles events during each loop cycle, depends on arm control mode
+         */
         override fun onLoop() {
             synchronized(this@Arm) {
                 if ( !useVelocityControl) {
