f24e5dc6dd19483bb0e9088b97826c1524bfdd15
Rithvik <rbhogavilli@gmail.com>
2018-01-24 16:50:27 -0500

    Create wrist code

--- src/main/java/org/usfirst/frc/team4099/robot/subsystems/Wrist.kt
+++ src/main/java/org/usfirst/frc/team4099/robot/subsystems/Wrist.kt
@@ -1,19 +1,30 @@
 package org.usfirst.frc.team4099.robot.subsystems
+import com.ctre.phoenix.motorcontrol.ControlMode
 import org.usfirst.frc.team4099.robot.Constants
 
-import edu.wpi.first.wpilibj.Talon
+import com.ctre.phoenix.motorcontrol.can.TalonSRX
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import org.usfirst.frc.team4099.robot.loops.Loop
 
 class Wrist private constructor(): Subsystem {
-    private val wristSRX = Talon(Constants.Wrist.WRIST_TALON_ID)
+    private val wristSRX = TalonSRX(Constants.Wrist.WRIST_TALON_ID)
+    private val arm = Arm.instance
 
     var wristState = WristState.HORIZONTAL
     private var wristPower = 0.0
+    private var wristAngle = 0.0
 
-    enum class WristState {
-        STOWED_DOWN, HORIZONTAL, STOWED_UP
+
+    enum class WristState(wristAngle: Double) {
+        STOWED_DOWN(-7*Math.PI/6),
+        HORIZONTAL(Double.NaN),
+        STOWED_UP(7*Math.PI)
     }
+
+    init {
+        wristSRX.set(ControlMode.MotionMagic, 0.0)
+    }
+
 
     override fun outputToSmartDashboard() {
         SmartDashboard.putNumber("wristPower", wristPower)
@@ -24,12 +35,12 @@ class Wrist private constructor(): Subsystem {
         setWristPower(0.0)
     }
 
-    fun setWristMode(state: WristState) {
+    private fun setWristMode(state: WristState) {
         wristState = state
     }
 
-    fun setWristPower(power: Double) {
-        wristSRX.set(power)
+    private fun setWristPower(power: Double) {
+        wristSRX.set(ControlMode.MotionMagic, power)
     }
 
     val loop: Loop = object : Loop {
@@ -38,18 +49,11 @@ class Wrist private constructor(): Subsystem {
         }
 
         override fun onLoop() {
+            if (wristState == WristState.HORIZONTAL) {
+                wristAngle = 2*Math.PI - arm.armAngle
+            }
             synchronized(this@Wrist) {
-                when (wristState) {
-                    Wrist.WristState.HORIZONTAL -> {
-                        setWristPower(0.0)
-                    }
-                    Wrist.WristState.STOWED_UP -> {
-                        setWristPower(1.0)
-                    }
-                    Wrist.WristState.STOWED_DOWN -> {
-                        setWristPower(-1.0)
-                    }
-                }
+                wristSRX.set(ControlMode.MotionMagic, WristConversion.radiansToPulses(wristAngle).toDouble())
             }
         }
 
