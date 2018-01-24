2dcff5878f1cc662dedb2e62a5e4d3c2b5101d42
Oksana <tkachoksn@gmail.com>
2018-01-24 17:30:58 -0500

    Add theoretical bot offset values, change all distances to inches, and add RobotProfile methods for offset

---
+++ src/main/java/org/usfirst/frc/team4099/paths/profiles/OurBot.kt
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team4099.paths.profiles
+
+/**
+ * Created by O on 1/24/2018.
+ */
+
+class OurBot : RobotProfile{
+    override fun getRedSwitchXCorrection(): Double {
+        return 0.0
+    }
+
+    override fun getRedSwitchYCorrection(): Double {
+        return 0.0    }
+
+    override fun getBlueSwitchXCorrection(): Double {
+        return 0.0
+    }
+
+    override fun getBlueSwitchYCorrection(): Double {
+        return 0.0
+    }
+
+}
