b863832c3dc06a495d295a9ebe15e04b3740831a
Sreedatta Kaligotla <33785968+SreedattaKaligotla@users.noreply.github.com>
2018-01-31 15:59:07 -0500

    made the *cylinders go forward
    made the *cylinders go forward in the init

--- src/main/java/org/usfirst/frc/team4099/robot/subsystems/ramp.kt
+++ src/main/java/org/usfirst/frc/team4099/robot/subsystems/ramp.kt
@@ -8,18 +8,18 @@ import org.usfirst.frc.team4099.robot.loops.Loop
 
 
 class ramp private constructor() : Subsystem {
-    private val pneumaticShifter1: Solenoid = Solenoid(Constants.Drive.SHIFTER_MODULE,Constants.Drive.SHIFTER_CHANNEL)
-    private val pneumaticShifter2: Solenoid = Solenoid(Constants.Drive.SHIFTER_MODULE,Constants.Drive.SHIFTER_CHANNEL)
-    private val pneumaticShifter3: Solenoid = Solenoid(Constants.Drive.SHIFTER_MODULE,Constants.Drive.SHIFTER_CHANNEL)
-    private val pneumaticShifter4: Solenoid = Solenoid(Constants.Drive.SHIFTER_MODULE,Constants.Drive.SHIFTER_CHANNEL)
+    private val pneumaticcylinder1: Solenoid = Solenoid(Constants.Drive.CYLINDER_MODULE,Constants.Drive.CYLINDER_CHANNEL)
+    private val pneumaticcylinder2: Solenoid = Solenoid(Constants.Drive.CYLINDER_MODULE,Constants.Drive.CYLINDER_CHANNEL)
+    private val pneumaticcylinder3: Solenoid = Solenoid(Constants.Drive.CYLINDER_MODULE,Constants.Drive.CYLINDER_CHANNEL)
+    private val pneumaticcylinder4: Solenoid = Solenoid(Constants.Drive.CYLINDER_MODULE,Constants.Drive.CYLINDER_CHANNEL)
 
 
 }
 init{
-    pneumaticShifter1.set(DoubleSolenoid.Value.kForward)
-    pneumaticShifter2.set(DoubleSolenoid.Value.kForward)
-    pneumaticShifter3.set(DoubleSolenoid.Value.kForward)
-    pneumaticShifter4.set(DoubleSolenoid.Value.kForward)
+    pneumaticcylinder1.set(DoubleSolenoid.Value.kForward)
+    pneumaticcylinder2.set(DoubleSolenoid.Value.kForward)
+    pneumaticcylinder3.set(DoubleSolenoid.Value.kForward)
+    pneumaticcylinder4.set(DoubleSolenoid.Value.kForward)
 
 }
 
