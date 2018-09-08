0e20d7612948fb060d54c696f600dbfcedb75591
azhong2002 <35146067+azhong2002@users.noreply.github.com>
2018-09-08 15:10:27 -0400

    Created PathGenerator class

---
+++ src/main/kotlin/org/usfirst/frc/team4099/auto/motionprofiling/PathGenerator.kt
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team4099.auto.motionprofiling
+
+import jaci.pathfinder.*
+
+/**
+ * Created by azhong2002 on 9/8/2018.
+ */
+
+class PathGenerator {
+
+    fun generatePath(waypoints : WayPoint[])
+}
